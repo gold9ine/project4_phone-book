ea4552f3cd5968c896b803b2ccb665e20cb6a95b
gold9ine <gold9ine@gmail.com>
2023-08-26 14:08:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search 창이 있다.
+// 2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
+// 3. 리스트에 아이템이 몇 개 있는지 보인다.
+// 4. 사용자가 유저를 이름 검색으로 찾을 수 있다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
